f16a113a81d275a13f4001cb16c5f40e65af9980
lmy <2679996800@qq.com>
2022-05-12 18:45:51 +0800

    添加卡牌详情弹窗

--- src/view/Swap.tsx
+++ src/view/Swap.tsx
@@ -1,10 +1,13 @@
 import React from "react"
+import CardDetails from '../components/CardDetails'
+import '../assets/style/carddetails.scss'
 
 function Swap() {
   return (
     <div>
       <div className="Edition-Center">  
         交易场
+        <CardDetails></CardDetails>
       </div>
     </div>
   )
