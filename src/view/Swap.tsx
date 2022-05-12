2fbd97c1c40a42c9be7fec85b3b1814df0c08614
李城龙 <2970174582@qq.com>
2022-05-12 18:56:16 +0800

    提交盲盒页面，交易场页面

--- src/view/Swap.tsx
+++ src/view/Swap.tsx
@@ -1,10 +1,29 @@
 import React from "react"
+import DropDown from '../components/DropDown'
+import CardItem from '../components/CardItem'
+import '../assets/style/Swap.scss'
 
 function Swap() {
   return (
     <div>
       <div className="Edition-Center">  
-        交易场
+        <div className="SwapTitle">
+        交易市場
+        </div>
+        <div className="screen">
+            <div className="Tabs">
+                <div className="activeTab linear-gradient">所有</div>
+                <div className="invalidTab">我的</div>
+            </div>
+            <div className="DropDownGroup">
+                <DropDown></DropDown>
+                <DropDown></DropDown>
+                <DropDown></DropDown>
+            </div>
+        </div>
+        <div className="CardList">
+            <CardItem></CardItem>
+        </div>
       </div>
     </div>
   )
