db19da2d34a220d36ea1e3d3efab3a367c14f194
lmy <2679996800@qq.com>
2022-05-20 11:46:29 +0800

    首页

--- src/components/CardDetails.tsx
+++ src/components/CardDetails.tsx
@@ -1,105 +1,148 @@
-import React, { useEffect , useState} from 'react';
-import {Contracts} from '../web3'
-import {useWeb3React} from '@web3-react/core'
-import {CardInfoType} from './Card'
-import {addMessage,showLoding} from '../utils/tool'
-import { Modal} from 'antd';
-import {contractAddress} from '../config'
-import '../assets/style/componentsStyle/carddetails.scss'
+import React, { useEffect, useState } from "react";
+import { Contracts } from "../web3";
+import { useWeb3React } from "@web3-react/core";
+import { CardInfoType } from "./Card";
+import { addMessage, showLoding } from "../utils/tool";
+import { Modal } from "antd";
+import { contractAddress } from "../config";
+import "../assets/style/componentsStyle/carddetails.scss";
 
-interface CardDetailPropsType{
-  isShow:boolean,
-  close:Function,
-  type:string,
-  CardInfo:CardInfoType,
-  showCreateOrder?:Function
-  showMerge?:Function
+interface CardDetailPropsType {
+  isShow: boolean;
+  close: Function;
+  type: string;
+  CardInfo: CardInfoType;
+  showCreateOrder?: Function;
+  showMerge?: Function;
 }
 /* type:Swap 交易场详情 CreateOrder 挂单详情 NFT 背包卡牌详情 */
- function CardDetails(props:CardDetailPropsType) {
-  const web3React = useWeb3React()
-  let [isApproved,setIsApproved] = useState(false)
-  let [putPrice,setPutPrice] = useState('0')
-   function createOrder(){
-     if(!web3React.account){
-      addMessage("请链接钱包")
-     }
-     showLoding(true)
-    Contracts.example.createOrder(web3React.account as string,props.CardInfo.tokenId,putPrice,'0x0000000000000000000000000000000000000000',contractAddress.NFT).then((res:any)=>{
-      addMessage("创建订单成功")
-      props.close()
-    }).finally(()=>{
-      showLoding(false)
-    })
-   }
-   useEffect(()=>{
-     if(web3React.account && props.type === "CreateOrder"){
-       console.log("获取授权状态")
-      Contracts.example.isApprovedForAll(web3React.account,contractAddress.EXChangeNFT).then((res:any)=>{
-        setIsApproved(res)
-        console.log(res,"授权状态")
+function CardDetails(props: CardDetailPropsType) {
+  const web3React = useWeb3React();
+  let [isApproved, setIsApproved] = useState(false);
+  let [putPrice, setPutPrice] = useState("0");
+  function createOrder() {
+    if (!web3React.account) {
+      addMessage("请链接钱包");
+    }
+    showLoding(true);
+    Contracts.example
+      .createOrder(
+        web3React.account as string,
+        props.CardInfo.tokenId,
+        putPrice,
+        "0x0000000000000000000000000000000000000000",
+        contractAddress.NFT
+      )
+      .then((res: any) => {
+        addMessage("创建订单成功");
+        props.close();
       })
-     }
-   },[web3React.account,props.type])
-   function Approval(){
-    if(!web3React.account){
-      addMessage("请链接钱包")
+      .finally(() => {
+        showLoding(false);
+      });
+  }
+  useEffect(() => {
+    if (web3React.account && props.type === "CreateOrder") {
+      console.log("获取授权状态");
+      Contracts.example
+        .isApprovedForAll(web3React.account, contractAddress.EXChangeNFT)
+        .then((res: any) => {
+          setIsApproved(res);
+          console.log(res, "授权状态");
+        });
+    }
+  }, [web3React.account, props.type]);
+  function Approval() {
+    if (!web3React.account) {
+      addMessage("请链接钱包");
     }
     /* 判断卡牌等级 */
-    Contracts.example.setApprovalForAll(web3React.account as string,contractAddress.EXChangeNFT,true).then(()=>{
-      setIsApproved(true)
-      addMessage("授权成功")
-    })
-   }
-   function putNum(e:React.ChangeEvent<HTMLInputElement>){
-    setPutPrice(e.target.value)
-   }
+    Contracts.example
+      .setApprovalForAll(
+        web3React.account as string,
+        contractAddress.EXChangeNFT,
+        true
+      )
+      .then(() => {
+        setIsApproved(true);
+        addMessage("授权成功");
+      });
+  }
+  function putNum(e: React.ChangeEvent<HTMLInputElement>) {
+    setPutPrice(e.target.value);
+  }
   return (
     <>
-    {/* <div className='box'>11111</div> */}
-      <Modal title="Basic Modal" visible={props.isShow} 
-      onCancel={()=>props.close()}
-      className='Card'
-      centered
-      width={'449px'}
-      closable={ false }
-      footer={null}
+      {/* <div className='box'>11111</div> */}
+      <Modal
+        title="Basic Modal"
+        visible={props.isShow}
+        // visible={true}
+        onCancel={() => props.close()}
+        className="Card"
+        centered
+        width={"449px"}
+        closable={false}
+        footer={null}
       >
-          <p className='title'>卡牌详情</p>
-          <div className='hzimg'>
-              <img src={props.CardInfo.imageUrl} alt=""></img>
+        <p className="title">卡牌详情</p>
+        <div className="hzimg">
+          <img src={props.CardInfo.imageUrl} alt=""></img>
+        </div>
+        <p className="kpdetails">卡牌名称:{props.CardInfo.cardName}</p>
+        <p className="kpdetails">卡牌ID:{props.CardInfo.id}</p>
+        <p className="kpdetails">卡牌等级:1星</p>
+        <p className="kpdetails">卡牌类型:无</p>
+        <p className="kpdetails">卡牌介绍:{props.CardInfo.introduce}</p>
+        {props.type === "CreateOrder" && (
+          <p className="kpdetails">
+            请输入价格:
+            <input type="number" onChange={putNum} />
+            BNB
+          </p>
+        )}
+        {props.type === "NFT" && (
+          <div className="butm">
+            <button className="gm">
+              <div
+                onClick={() => {
+                  props.showCreateOrder && props.showCreateOrder();
+                }}
+              >
+                挂卖
+              </div>
+            </button>
+            <button
+              className="hc"
+              onClick={() => {
+                props.showMerge && props.showMerge();
+              }}
+            >
+              合成
+            </button>
+            <button className="zy">质押</button>
           </div>
-          <p className='kpdetails'>卡牌名称:{props.CardInfo.cardName}</p>
-          <p className='kpdetails'>卡牌ID:{props.CardInfo.id}</p>
-          <p className='kpdetails'>卡牌等级:1星</p>
-          <p className='kpdetails'>卡牌类型:无</p>
-          <p className='kpdetails'>卡牌介绍:{props.CardInfo.introduce}</p>
-          {
-            props.type === "CreateOrder" &&<p className='kpdetails'>请输入价格:<input type='number' onChange={putNum}/>BNB</p>
-          }
-          {
-            props.type === "NFT" && <div className='butm'>
-                <button className='gm'><div onClick={()=>{props.showCreateOrder && props.showCreateOrder()}}>挂卖</div></button>
-                <button className='hc' onClick={()=>{props.showMerge && props.showMerge()}}>合成</button>
-                <button className='zy'>质押</button>
-            </div>
-          }
-          {
-            props.type === "CreateOrder" && <div className='butm'>
-              {
-                isApproved ? <>
-                <button className='hc' onClick={createOrder}>确认</button>
-                </>:<>
-                <button className='hc' onClick={Approval}>授权</button>
-                </>
-              }
-                
-                
-            </div>
-          }
-          <span>点击任意地方离开</span>
-        </Modal>
+        )}
+        {props.type === "CreateOrder" && (
+          <div className="butm">
+            {isApproved ? (
+              <>
+                <button className="hc" onClick={createOrder}>
+                  确认
+                </button>
+              </>
+            ) : (
+              <>
+                <button className="hc" onClick={Approval}>
+                  授权
+                </button>
+              </>
+            )}
+          </div>
+        )}
+        <span>点击任意地方离开</span>
+      </Modal>
     </>
-  )
+  );
 }
-export default CardDetails
+export default CardDetails;
--- src/view/NFT.tsx
+++ src/view/NFT.tsx
@@ -1,182 +1,198 @@
-import React,{useState , useEffect} from "react"
-import '../assets/style/componentsStyle/carddetails.scss'
-import {useSelector , useDispatch} from "react-redux";
-import {stateType} from '../store/reducer'
-import {useWeb3React} from '@web3-react/core'
-import CardDetails from '../components/CardDetails'
-import {getBoxUserInfo , getUserCard} from '../API'
-import DropDown from '../components/DropDown'
-import Card,{CardInfoType} from '../components/Card'
-import BlindBox from '../components/BlindBox'
-import CardSynthesis from "../components/CardSynthesis"
-import '../assets/style/Swap.scss'
-import '../assets/style/componentsStyle/CardSynthesis.scss'
-import NoData from '../components/NoData'
-import AddFlow from '../components/AddFlow'
-import {addMessage,showLoding} from '../utils/tool'
-import { Pagination } from 'antd';
-import '../assets/style/componentsStyle/AddFlow.scss'
-import AddFluidOk from "../components/AddFluidOk"
-import '../assets/style/componentsStyle/AddFluidOk.scss'
+import React, { useState, useEffect } from "react";
+import "../assets/style/componentsStyle/carddetails.scss";
+import { useSelector, useDispatch } from "react-redux";
+import { stateType } from "../store/reducer";
+import { useWeb3React } from "@web3-react/core";
+import CardDetails from "../components/CardDetails";
+import { getBoxUserInfo, getUserCard } from "../API";
+import DropDown from "../components/DropDown";
+import Card, { CardInfoType } from "../components/Card";
+import BlindBox from "../components/BlindBox";
+import CardSynthesis from "../components/CardSynthesis";
+import "../assets/style/Swap.scss";
+import "../assets/style/componentsStyle/CardSynthesis.scss";
+import NoData from "../components/NoData";
+import AddFlow from "../components/AddFlow";
+import { addMessage, showLoding } from "../utils/tool";
+import { Pagination } from "antd";
+import "../assets/style/componentsStyle/AddFlow.scss";
+import AddFluidOk from "../components/AddFluidOk";
+import "../assets/style/componentsStyle/AddFluidOk.scss";
 // 挂卖详情
-import PutParticulars from '../components/PutParticulars'
+import PutParticulars from "../components/PutParticulars";
 // 合成成功
-import ComSucceed from '../components/ComSucceed'
+import ComSucceed from "../components/ComSucceed";
 // 卡牌合成规则
-import CardComRule from '../components/CardComRule'
+import CardComRule from "../components/CardComRule";
 // 盲盒开启
-import BoxOpen from '../components/BoxOpen'
+import BoxOpen from "../components/BoxOpen";
 
-
-
-export interface BoxInfo{
-  id:number,
+export interface BoxInfo {
+  id: number;
 }
-export interface OpenResType{
-  imageUrl:string,
-  cardLevel:number,
-  cardName:string
+export interface OpenResType {
+  imageUrl: string;
+  cardLevel: number;
+  cardName: string;
 }
 const LevelMap = [
   {
-    key:'全部等级',
-    value:0
+    key: "全部等级",
+    value: 0,
   },
   {
-    key:'普通',
-    value:1
+    key: "普通",
+    value: 1,
   },
   {
-    key:'良好',
-    value:2
+    key: "良好",
+    value: 2,
   },
   {
-    key:'优秀',
-    value:3
+    key: "优秀",
+    value: 3,
   },
   {
-    key:'稀有',
-    value:4
+    key: "稀有",
+    value: 4,
   },
   {
-    key:'良品',
-    value:5
+    key: "良品",
+    value: 5,
   },
   {
-    key:'史诗',
-    value:6
-  }
-]
+    key: "史诗",
+    value: 6,
+  },
+];
 const typeMap = [
   {
-    key:'全部类型',
-    value:0
+    key: "全部类型",
+    value: 0,
   },
   {
-    key:'类型1',
-    value:1
+    key: "类型1",
+    value: 1,
   },
   {
-    key:'类型2',
-    value:2
+    key: "类型2",
+    value: 2,
   },
   {
-    key:'类型3',
-    value:3
+    key: "类型3",
+    value: 3,
   },
   {
-    key:'类型4',
-    value:4
-  }
-]
+    key: "类型4",
+    value: 4,
+  },
+];
 function NFT() {
-    let state = useSelector<stateType,stateType>(state => state);
-    const web3React = useWeb3React()
-    let [TabIndex,SetTabIndex] = useState(0)
-    /* 类型筛选 */
-    let [type,SetType] = useState(0)
-    /* 等级筛选 */
-    let [level,SetLevel] = useState(0)
-    /* 分页总条数 */
-    let [totalNum,SetTotalNum] = useState(0)
-    let [page,SetPage] = useState(1)
-    let [cardDetialIndex,setCardDetialIndex] = useState(0)
-    let [userBox,setuserBox] = useState<BoxInfo []>([])
-    let [userCard,setuserCard] = useState<CardInfoType []>([])
-    /* 合成弹窗控制 */
-    const [showCardSynthesis, setshowCardSynthesis] = useState(false)
-    /* 卡牌详情弹窗控制 */
-    let [showCardDetail,setShowCardDetail] = useState(false)
-    /* 创建订单弹窗控制 */
-    let [showCreateOrder,setShowCreateOrder] = useState(false)
-    /* 开卡成功弹窗控制 */
-    let [showOpenCard,setShowOpenCard] = useState(false)
-    /* 开盲盒结果 */
-    let [openRes,setOpenRes] = useState<OpenResType | null>(null)
-    function showDetial(index:number) {
-      setCardDetialIndex(index)
-      setShowCardDetail(true)
+  let state = useSelector<stateType, stateType>((state) => state);
+  const web3React = useWeb3React();
+  let [TabIndex, SetTabIndex] = useState(0);
+  /* 类型筛选 */
+  let [type, SetType] = useState(0);
+  /* 等级筛选 */
+  let [level, SetLevel] = useState(0);
+  /* 分页总条数 */
+  let [totalNum, SetTotalNum] = useState(0);
+  let [page, SetPage] = useState(1);
+  let [cardDetialIndex, setCardDetialIndex] = useState(0);
+  let [userBox, setuserBox] = useState<BoxInfo[]>([]);
+  let [userCard, setuserCard] = useState<CardInfoType[]>([]);
+  /* 合成弹窗控制 */
+  const [showCardSynthesis, setshowCardSynthesis] = useState(false);
+  /* 卡牌详情弹窗控制 */
+  let [showCardDetail, setShowCardDetail] = useState(false);
+  /* 创建订单弹窗控制 */
+  let [showCreateOrder, setShowCreateOrder] = useState(false);
+  /* 开卡成功弹窗控制 */
+  let [showOpenCard, setShowOpenCard] = useState(false);
+  /* 开盲盒结果 */
+  let [openRes, setOpenRes] = useState<OpenResType | null>(null);
+  function showDetial(index: number) {
+    setCardDetialIndex(index);
+    setShowCardDetail(true);
+  }
+  function onChange(pageNumber: number) {
+    SetPage(pageNumber);
+    console.log("Page: ", pageNumber);
+  }
+  function createOrderFun() {
+    setShowCardDetail(false);
+    setShowCreateOrder(true);
+  }
+  function showMergeFun() {
+    setShowCardDetail(false);
+    setshowCardSynthesis(true);
+  }
+  function openSuccess(res: OpenResType) {
+    setOpenRes(res);
+    setShowOpenCard(true);
+  }
+  /* 初始化数据 */
+  useEffect(() => {
+    if (state.token && web3React.account) {
+      getBoxUserInfo(1, 10).then((res) => {
+        setuserBox(res.data);
+        console.log(res, "用户盲盒信息");
+      });
     }
-    function onChange(pageNumber:number) {
-      SetPage(pageNumber)
-      console.log('Page: ', pageNumber);
+  }, [state.token, web3React.account]);
+  useEffect(() => {
+    if (state.token && web3React.account) {
+      getUserCard({
+        currentPage: page,
+        level: level,
+        pageSize: 10,
+        type: type,
+        userAddress: web3React.account,
+      }).then((res) => {
+        setuserCard(res.data.list);
+        SetTotalNum(res.data.size);
+        console.log(res, "获取用户所有卡牌");
+      });
     }
-    function createOrderFun(){
-      setShowCardDetail(false)
-      setShowCreateOrder(true)
-    }
-    function showMergeFun(){
-      setShowCardDetail(false)
-      setshowCardSynthesis(true)
-    }
-    function openSuccess(res:OpenResType){
-      setOpenRes(res)
-      setShowOpenCard(true)
-    }
-    /* 初始化数据 */
-    useEffect(()=>{
-      if(state.token && web3React.account){
-        getBoxUserInfo(1,10).then(res=>{
-          setuserBox(res.data)
-          console.log(res,"用户盲盒信息")
-        })
-      }
-    },[state.token,web3React.account])
-    useEffect(()=>{
-      if(state.token && web3React.account){
-        getUserCard({
-          currentPage:page,
-          level:level,
-          pageSize:10,
-          type:type,
-          userAddress:web3React.account
-        }).then(res=>{
-          setuserCard(res.data.list)
-          SetTotalNum(res.data.size)
-          console.log(res,"获取用户所有卡牌")
-        })
-      }
-    },[state.token,web3React.account,type,level,page])
+  }, [state.token, web3React.account, type, level, page]);
   return (
     <div>
       {/* <AddFlow></AddFlow> */}
-      {
-        openRes && <ComSucceed isShow={showOpenCard} close={()=>setShowOpenCard(false)} CardInfo={openRes} ></ComSucceed>
-      }
+      {openRes && (
+        <ComSucceed
+          isShow={showOpenCard}
+          close={() => setShowOpenCard(false)}
+          CardInfo={openRes}
+        ></ComSucceed>
+      )}
       {/* 卡牌详情 */}
-      {
-        userCard.length >0 && <CardDetails isShow={showCardDetail} showMerge={showMergeFun} showCreateOrder={createOrderFun} CardInfo={userCard[cardDetialIndex]} close={()=>setShowCardDetail(false)} type="NFT"></CardDetails>
-      }
+      {userCard.length > 0 && (
+        <CardDetails
+          isShow={showCardDetail}
+          showMerge={showMergeFun}
+          showCreateOrder={createOrderFun}
+          CardInfo={userCard[cardDetialIndex]}
+          close={() => setShowCardDetail(false)}
+          type="NFT"
+        ></CardDetails>
+      )}
       {/* 卡牌挂卖 */}
-      {
-        userCard.length >0  && <CardDetails isShow={showCreateOrder} CardInfo={userCard[cardDetialIndex]} close={()=>setShowCreateOrder(false)} type="CreateOrder"></CardDetails>
-      }
+      {userCard.length > 0 && (
+        <CardDetails
+          isShow={showCreateOrder}
+          CardInfo={userCard[cardDetialIndex]}
+          close={() => setShowCreateOrder(false)}
+          type="CreateOrder"
+        ></CardDetails>
+      )}
       {/* 卡牌合成 */}
-      <CardSynthesis isShow={showCardSynthesis} CardInfo={userCard[cardDetialIndex]} close={()=>setshowCardSynthesis(false)}></CardSynthesis>
+      <CardSynthesis
+        isShow={showCardSynthesis}
+        CardInfo={userCard[cardDetialIndex]}
+        close={() => setshowCardSynthesis(false)}
+      ></CardSynthesis>
       <div className="Edition-Center">
-        <div className="SwapTitle">
-          NFT - 庫存
-        </div>
+        <div className="SwapTitle">NFT - 庫存</div>
         {/* 盲盒开启 */}
         <BoxOpen></BoxOpen>
         {/* 卡牌合成规则 */}
@@ -186,53 +202,92 @@ function NFT() {
         {/* 合成成功 */}
         {/* <ComSucceed></ComSucceed> */}
         <div className="screen">
-            <div className="Tabs">
-                <div className={TabIndex === 0 ? 'activeTab linear-gradient':'invalidTab'} onClick={() =>{SetTabIndex(0)}}>卡牌</div>
-                <div className={TabIndex === 1 ? 'activeTab linear-gradient':'invalidTab'} onClick={() =>{SetTabIndex(1)}}>盲盒</div>
+          <div className="Tabs">
+            <div
+              className={
+                TabIndex === 0 ? "activeTab linear-gradient" : "invalidTab"
+              }
+              onClick={() => {
+                SetTabIndex(0);
+              }}
+            >
+              卡牌
             </div>
-            {
-              TabIndex === 0  && <div className="DropDownGroup">
-                  <DropDown Map={LevelMap} change={SetLevel}></DropDown>
-                  <DropDown Map={typeMap} change={SetType}></DropDown>
-              </div>
-            }
-            
+            <div
+              className={
+                TabIndex === 1 ? "activeTab linear-gradient" : "invalidTab"
+              }
+              onClick={() => {
+                SetTabIndex(1);
+              }}
+            >
+              盲盒
+            </div>
+          </div>
+          {TabIndex === 0 && (
+            <div className="DropDownGroup">
+              <DropDown Map={LevelMap} change={SetLevel}></DropDown>
+              <DropDown Map={typeMap} change={SetType}></DropDown>
+            </div>
+          )}
         </div>
-        {
-          TabIndex === 0 ? <>
+        {TabIndex === 0 ? (
+          <>
             {/* 卡牌 */}
-            {
-              userCard.length !==0 ? <>
+            {userCard.length !== 0 ? (
+              <>
                 <div className="CardList">
-                  {
-                    userCard.map((item,index)=><Card key={item.id} Index={index} cardInfo={item} showDetia={showDetial}></Card>)
-                  }
+                  {userCard.map((item, index) => (
+                    <Card
+                      key={item.id}
+                      Index={index}
+                      cardInfo={item}
+                      showDetia={showDetial}
+                    ></Card>
+                  ))}
                 </div>
-              </>:<>
+              </>
+            ) : (
+              <>
                 <NoData></NoData>
               </>
-            }
-              <div className="Pagination">
-                  <Pagination style={{margin:"auto"}} showQuickJumper defaultCurrent={page} hideOnSinglePage showSizeChanger={false} total={totalNum} onChange={onChange} />
-              </div>
-            </> : <>
-            {/* 盲盒 */}
-            {
-              userBox.length !==0 ? <>
-                <div className="CardList">
-                    {
-                      userBox.map((item)=><BlindBox openSuccess={openSuccess} key={item.id} BoxInfo={item}></BlindBox>)
-                    }
-                </div>
-              </>:<>
-                <NoData></NoData>
-              </>
-            }
-            
+            )}
+            <div className="Pagination">
+              <Pagination
+                style={{ margin: "auto" }}
+                showQuickJumper
+                defaultCurrent={page}
+                hideOnSinglePage
+                showSizeChanger={false}
+                total={totalNum}
+                onChange={onChange}
+              />
+            </div>
           </>
-        }
+        ) : (
+          <>
+            {/* 盲盒 */}
+            {userBox.length !== 0 ? (
+              <>
+                <div className="CardList">
+                  {userBox.map((item) => (
+                    <BlindBox
+                      openSuccess={openSuccess}
+                      key={item.id}
+                      BoxInfo={item}
+                    ></BlindBox>
+                  ))}
+                </div>
+              </>
+            ) : (
+              <>
+                <NoData></NoData>
+              </>
+            )}
+          </>
+        )}
       </div>
     </div>
-  )
+  );
 }
-export default React.memo(NFT)
+export default React.memo(NFT);
