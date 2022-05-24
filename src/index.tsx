8030ce07174b4fe2108960c55436df3119fbd27b
lmy <2679996800@qq.com>
2022-05-24 10:43:42 +0800

    首页

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 // import { Web3Provider } from "@ethersproject/providers";
-import ReactDOM from 'react-dom';
-import './index.css';
-import {Provider} from "react-redux"; 
-import { Web3ReactProvider } from '@web3-react/core'
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Provider } from "react-redux";
+import { Web3ReactProvider } from "@web3-react/core";
 import store from "./store";
-import Web3 from 'web3'
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { HashRouter} from "react-router-dom";
-import {Contracts} from './web3'
-import 'antd/dist/antd.min.css';
-function getLibrary(provider :any) : Web3{
+import Web3 from "web3";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { HashRouter } from "react-router-dom";
+import { Contracts } from "./web3";
+import "antd/dist/antd.min.css";
+function getLibrary(provider: any): Web3 {
   const library = new Web3(provider);
-  new Contracts(provider)
+  new Contracts(provider);
   return library;
 }
 ReactDOM.render(
@@ -21,12 +21,12 @@ ReactDOM.render(
     <Provider store={store}>
       <Web3ReactProvider getLibrary={getLibrary}>
         <HashRouter>
-          <App/>
+          <App />
         </HashRouter>
       </Web3ReactProvider>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
